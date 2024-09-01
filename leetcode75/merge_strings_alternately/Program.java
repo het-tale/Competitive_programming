1e7d9cf27eb32149df065b920ab497c581cb4d7e
het-tale <ettalebyhasnaa99@gmail.com>
2024-09-02 00:07:55 +0100

    feat: solve the merge strings alternately challenge

---
+++ leetcode75/merge_strings_alternately/Program.java
@@ -0,0 +1,9 @@
+public class Program {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String word1 = "abc";
+        String word2 = "pqr";
+        String result = solution.mergeAlternately(word1, word2);
+        System.out.println(result);
+    }
+}
